5e59265195401896b2f82c3afa9fadfa4b7612d6
ChunShuiZi <xzsjy123456@163.com>
2024-04-10 17:40:29 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	new file:   public/ShaperLogo.png
    	deleted:    public/vite.svg
    	modified:   src/App.vue
    	new file:   src/assets/HarmonyOS_Sans_Light.ttf
    	new file:   src/assets/HarmonyOS_Sans_Medium.ttf
    	new file:   src/assets/ShaperLogo.png
    	new file:   src/assets/github.png
    	deleted:    src/assets/vue.svg
    	deleted:    src/components/HelloWorld.vue
    	new file:   src/components/MainWeb.vue
    	new file:   src/components/WebFooter.vue
    	new file:   src/components/WebHeader.vue
    	modified:   src/main.js
    	deleted:    src/style.css

---
+++ src/components/MainWeb.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WebHeader from './WebHeader.vue';
+</script>
+
+<template>
+  <WebHeader />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/WebHeader.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="HeaderTop"> 
+  <div class="ShaperTop">
+  <img src= "../assets/ShaperLogo.png"  class="ShaperLogo">
+  </img>
+  <div class="ShaperTitle">VocalShaper</div>
+   <div class="VOCALSHAPER">VOCALSHAPER</div>
+   <div class="DOWNLOAD">DOWNLOAD</div>
+   <div class="DOCUMENT">DOCUMENT</div>
+   <div class="CONTACT">CONTACT</div>
+   <img src= "../assets/github.png"  class="GithubLogo">
+  </img>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+@font-face {
+    font-family:TitleT;
+    src: url("../assets/HarmonyOS_Sans_Light.ttf");
+}
+.HeaderTop{
+    background: linear-gradient(to bottom, rgba(0,0,0,0.85),rgba(0,0,0,0));
+    width: 101.5%;
+    height: 90px;
+    margin: -1%;
+}
+.ShaperTop{
+  font-family: TitleT;letter-spacing:1.5px;
+}
+.ShaperLogo{
+    width: 35px; float: left;filter:opacity(75%);
+    margin-top: 27.5px;margin-left: 65px;
+}
+.ShaperTitle{
+    float: left;color: rgba(255, 255, 255, 0.65);
+    margin-top: 31px;margin-left: 20px;font-size: 20px;
+}
+.VOCALSHAPER{
+  float:left;margin-left: 240px;margin-top: 36px;font-size: 15px;color: rgba(255, 255, 255, 0.65);
+}
+.DOWNLOAD{
+  float:left;margin-left: 110px;margin-top: 36px;font-size: 15px;color: rgba(255, 255, 255, 0.65);
+}
+.DOCUMENT{
+  float:left;margin-left: 110px;margin-top: 36px;font-size: 15px;color: rgba(255, 255, 255, 0.65);
+}
+.CONTACT{
+  float:left;margin-left: 110px;margin-top: 36px;font-size: 15px;color: rgba(255, 255, 255, 0.65);
+}
+.GithubLogo{
+  float:left;width: 32px;
+  margin-top: 28px;
+  margin-left: 240px;
+  filter:opacity(75%);  
+  border:1.5px solid rgba(255, 255, 255, 0.50);
+  border-radius: 50px;
+}
+</style>
